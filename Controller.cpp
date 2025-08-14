5e0e9542a07d831f0c3d8867a0292b1c250296e7
Josias Kanyinda <jkkanyinda@gmail.com>
2025-08-14 13:47:07 -0400

    Added comments to controller class

--- Controller.cpp
+++ Controller.cpp
@@ -8,6 +8,9 @@ void Controller::tick() {
 	Direction currentState = elevator->getDirection();
 
 	switch (currentState) {
+		// when idle, it checked the requests if any has appeared.
+		// if so, it will change the currentState to the following direction,
+		// making it switch cases in its next tick.
 		case Direction::IDLE: {
 			if (!upRequests.empty()) {
 				std::cout << "Elevator has Up requests and now going up" << std::endl;
@@ -27,12 +30,19 @@ void Controller::tick() {
 				return;
 			}
 
+			// Elevator will stop at a floor that is currently on its request set.
+			// After procedure, it will delete the floor from the set.
 			if (upRequests.count(elevator->getCurrentFloor())) {
 				elevator->stop();
 				elevator->openDoors();
 				elevator->closeDoors();
 				upRequests.erase(elevator->getCurrentFloor());
 			}
+			// Knowing its not at a requested floor, it will continue going in its 
+			// direction if the highest/lowest call is more/less than the elevator
+			// current floor. (e.g. highest call is 10 and current floor is 4, the
+			// elevator will keep going till its current floor is 10) else, it will 
+			// go IDLE
 			else {
 				int highestRequest = *upRequests.rbegin();
 				if (elevator->getCurrentFloor() < highestRequest) {
@@ -45,6 +55,7 @@ void Controller::tick() {
 			break;
 		}
 
+		// explanation is the same from UP case.
 		case Direction::DOWN: {
 			if (downRequests.empty()) {
 				std::cout << "Elevator Stopping going Down" << std::endl;
