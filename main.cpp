33ce94263548f6503641e958e17533fa54c57128
Josias Kanyinda <jkkanyinda@gmail.com>
2025-08-15 19:23:06 -0400

    Added imgui

--- main.cpp
+++ main.cpp
@@ -1,19 +1,108 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include "Elevator.h"
 #include "Controller.h"
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 int main() {
+
+	// Initialize GLFW
+	glfwInit();
+
+
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
+
+	//IMGUI initalization
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Take care of all GLFW events
+		glfwPollEvents();
+
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//imgui setup
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		//imgui window
+		ImGui::Begin("Elevator");
+		ImGui::Text("YO WE DID IT!");
+		ImGui::End();
+
+		//imgui draw
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+
+
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
+
 	Elevator elevator(8);
 	Controller controller(&elevator);
 
 	controller.addFloorRequest(5, Direction::UP);
 	controller.addCarRequest(9);
 	controller.addFloorRequest(4, Direction::DOWN);
-	controller.addCarRequest(1);
-
-	while (true) {
-		controller.tick();
-	}
+	controller.addCarRequest(7);
 
 	return 0;
 }
